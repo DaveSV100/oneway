0ad2468460b7546731735e7fae09eb85bef4a635
DavidSaavedraVazquez <davidsv20@icloud.com>
2024-05-13 11:57:58 -0600

    Title and image

--- client/src/Components/Body/Index.jsx
+++ client/src/Components/Body/Index.jsx
@@ -8,19 +8,17 @@ const Body = () => {
 
     return (
         <body>
-            <img className='cover-picture' src="https://images.pexels.com/photos/443383/pexels-photo-443383.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"></img>
-            <div>
-                <ul className='titles flex justify-around'>
-                    <li><a href="">Quienes somos</a></li>
-                    <li><a href="#services">Services</a></li>
-                    <li><a href="">Contaco</a></li>
-                </ul>
+            <div className='top'>
+                <img className='cover-picture' src="https://images.pexels.com/photos/257856/pexels-photo-257856.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"></img>
+                <div className='title'>
+                    <h1>Mantenimiento de edificios</h1>
+                </div>
             </div>
-            <div id="us">
-                <h2 className=''>Quienes somos</h2>
+            <div id="us" >
+                <h2 className='subtitle'>Quienes somos</h2>
                 <p>
                     Somos una empresa joven con talento 100% mexicano dedicada a dar mantenimiento a
-                    elevadores de personal y carga. Contamos con amplia experiencia en elevadores de todas las marcas. Nuestro colaboradores reciben capacitación constantemente por los que te podemos garantizar una servicio totalmente profesional. Nos distingue la honestidad jamás hemos hecho ni haremos "reparaciones fantasma" y si un problema nos supera siempre buscamos la mejor solución. Trabajamos en la Ciudad de México y zona metropolitana, así como en el interior de toda la república.
+                    elevadores de personal y carga. Contamos con amplia experiencia en elevadores de todas las marcas. 
                 </p>
             </div>
             <div id='services'>
@@ -36,8 +34,10 @@ const Body = () => {
                     </ol>
                 </div>
             </div>
-            
-
+            <div id='whyUs'>
+                <h2>Por qué elegirnos</h2>
+                <p>Nuestro colaboradores reciben capacitación constantemente por los que te podemos garantizar una servicio totalmente profesional. Nos distingue la honestidad jamás hemos hecho ni haremos "reparaciones fantasma" y si un problema nos supera siempre buscamos la mejor solución. Trabajamos en la Ciudad de México y zona metropolitana, así como en el interior de toda la república.</p>
+            </div>
         </body>
     )
 }
